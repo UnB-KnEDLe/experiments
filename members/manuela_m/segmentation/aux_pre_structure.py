37edf813d7df6404d348013c431bd6382f028e8e
munak98 <42423690+munak98@users.noreply.github.com>
2022-05-16 15:07:21 -0300

    Add files via upload

---
+++ members/manuela_m/segmentation/aux_pre_structure.py
@@ -0,0 +1,123 @@
+from email.mime import text
+import glob
+import re
+from Bio.Align import PairwiseAligner
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import wordpunct_tokenize
+import re
+
+from aux_functions import *
+
+
+class LabelStructure:
+    """Class with static methods to create a labeled DODF structure by sentences"""
+
+    @staticmethod
+    def find_alignment(data, segment: str):
+        """Find the alignment between text from path_dodf and the text segment.
+        Args:
+            path_dodf ([type]): path to text file extracted from DODF
+            segment (str): text segmento to match in path_dodf document
+        Returns:
+            alignment
+        """
+        aligner = PairwiseAligner()
+        aligner.open_gap_score = -0.5
+        aligner.extend_gap_score = -0.1
+        aligner.target_end_gap_score = 0.0
+        aligner.query_end_gap_score = 0.0
+
+        alignment = None
+        alignment = next(aligner.align(data, segment))
+        return alignment
+
+    @staticmethod
+    def load_txt_by_numdodf(path_txt) -> dict:
+        """Load documents from a directory path and create a dictionary
+        Args:
+            path_txt ([type]): path to directory with *.txt
+        Returns:
+            dict: Dictionary with key value the number of DODF e values a list of text file path.
+        """
+        dic_dodfs = {}
+        for p in glob.glob(path_txt):
+            num_dodf = int(p.split()[1])
+            if num_dodf not in dic_dodfs:
+                dic_dodfs[num_dodf] = []
+            dic_dodfs[num_dodf].append(p)
+        return dic_dodfs
+
+    @staticmethod
+    def positions(
+        df, path_txt, column_name_num_dodf="NUM_DODF", columns_name_text="text"
+    ):
+        """[summary]
+        Args:
+            df ([type]): [description]
+            path_txt ([type]): [description]
+            column_name_num_dodf (str, optional): [description]. Defaults to 'NUM_DODF'.
+            columns_name_text (str, optional): [description]. Defaults to 'text'.
+        """
+        dic_dodfs = LabelStructure.load_txt_by_numdodf(path_txt)
+        act_pos = {}
+        for i in range(len(df)):
+            num_dodf = int(df.loc[i, column_name_num_dodf])
+            act = df.loc[i, columns_name_text]
+            paths_dodf = dic_dodfs[num_dodf]
+            for path in paths_dodf:
+                alig = LabelStructure.find_alignment(path, act)
+                if alig.score > len(act) - 10:
+                    init = alig.aligned[0][0][0]
+                    end = alig.aligned[0][0][1]
+                    act_pos[path] = act_pos.get(path, [])
+                    act_pos[path].append((i, act, init, end))
+        return act_pos
+
+    # segmenta o texto selecionando os atos e tudo entre os atos.
+    @staticmethod
+    def segmentor(text, pos_list):
+        pos_list.sort(key=lambda x: x[2])
+
+        init = 0
+        end = len(text)
+        segs = []
+        for pos in pos_list:
+            act = pos[1]
+            a_init = pos[2]
+            a_end = pos[3]
+            segs.append(text[init:a_init])
+            segs.append(act)
+            init = a_end + 1
+        segs.append(text[init:end])
+        return segs
+
+    @staticmethod
+    def sentence_labeling(segs, labels=["O", "B", "I"]):
+        iob = []
+        for i, txt in enumerate(segs):
+            lines = []
+            txt = re.sub(r"(\s+)", " ", txt)
+            sents = sent_tokenize(txt)
+            if i % 2 == 0:
+                lines = [labels[0] + " " + sent for sent in sents]
+            else:
+                lines += [labels[1] + " " + sents[0]]
+                lines += [labels[2] + " " + sent for sent in sents[1:]]
+
+            lines = "\n".join(lines)
+            iob.append(lines)
+
+        return "\n\n".join(iob)
+
+    @staticmethod
+    def word_labeling(segs, labels=["O", "B", "I"]):
+        label_sents = []
+        for i, txt in enumerate(segs):
+            txt = txt.replace("\n", " ")
+            sents = wordpunct_tokenize(txt)
+            if i % 2 == 0:
+                label_sents += [labels[0] + " " + sent for sent in sents]
+            else:
+                label_sents.append(labels[1] + " " + sents[0])
+                label_sents += [labels[2] + " " + sent for sent in sents[1:]]
+        return "\n\n".join(label_sents)
---
+++ members/manuela_m/segmentation/segmentation_flow.py
@@ -0,0 +1,157 @@
+"""
+This module implements a metaflow flow for training and evaluating a
+segmentation model using a LSTM+CRF.
+To run the flow, you must pass as arguments the type of act that the model will
+be trained for, the path to the trained embedding that will be used by the
+model, the path to the pre-processed dataset that will be used for training,
+validation, and testing, and the path to save the model. For example:
+python3 segmentation_flow.py run
+--act aviso_de_licitacao
+--embedding cbow_s50_2.txt
+--dataset Processed_dataset/segmentacao_aviso_de_licitacao.iob
+--output Models/
+The dataset must have been preprocessed by the PreProcessFlow flow.
+"""
+
+import unicodedata
+import numpy as np
+import torch
+from gensim.models import KeyedVectors
+from torch.utils.data import DataLoader
+from dataset_structure import SegmentationDataset
+from model_structure import LstmCrf
+from metaflow import FlowSpec, step, Parameter
+import pickle
+
+
+class SegmentationFlow(FlowSpec):
+    """
+    A flow to train and evaluate a LSTM+CRF model for segmentation.
+    """
+
+    act = Parameter(
+        "act",
+        help="Type of act to model (example: 'aviso de licitação'",
+        required=True,
+        type=str,
+    )
+
+    embedding = Parameter(
+        "embedding", help="Path to the trained embeding", required=True, type=str
+    )
+
+    dataset = Parameter("dataset", help="Path to the dataset", required=True, type=str)
+
+    output = Parameter(
+        "output", help="Path to store the output model", required=True, type=str
+    )
+
+    @step
+    def start(self):
+        """
+        Start setp, define the data splits and calls the next step.
+        """
+        self.data_splits = ["train", "validation", "test"]
+        self.next(self.prepare_embedings)
+
+    @step
+    def prepare_embedings(self):
+        """
+        Prepare the embeddings and tags dictionaries to transform the dataset in the next step
+        """
+        emb = KeyedVectors.load_word2vec_format(f"{self.embedding}")
+        word2idx = {}
+        for j in emb.index_to_key:
+            word = (
+                unicodedata.normalize("NFKD", j)
+                .encode("ascii", "ignore")
+                .decode("utf8")
+            )
+            word2idx[word] = emb.key_to_index[j]
+
+        self.tag2idx = {"B": 0, "I": 1, "O": 2}
+        self.idx2tag = {0: "B", 1: "I", 2: "O"}
+        self.emb = emb
+        self.word2idx = word2idx
+
+        self.next(self.load_data, foreach="data_splits")
+
+    @step
+    def load_data(self):
+        """
+        Creates an object of the SegmentationDataset class to load and
+        perform the necessary transformations on the dataset,
+        such as spliting train, test and validation sets,
+        truncating sentences and long blocks and padding them.
+        """
+        data = SegmentationDataset(
+            tag2idx=self.tag2idx,
+            word2idx=self.word2idx,
+            set_type=self.input,
+            path=self.dataset,
+            max_sequence_lenght=30,
+        )
+
+        self.data_loader = DataLoader(
+            dataset=data, batch_size=32, shuffle=False, num_workers=2, pin_memory=True
+        )
+        self.set_type = self.input
+        self.next(self.join)
+
+    @step
+    def join(self, inputs):
+        """
+        Merge the branches created in the previous step and store the data loaders in the dictionary
+        'self.data_dicts'.
+        """
+        self.data_dicts = {branch.set_type: branch.data_loader for branch in inputs}
+        self.merge_artifacts(inputs, exclude=["set_type", "data_loader"])
+        self.next(self.fit)
+
+    @step
+    def fit(self):
+        """
+        Train the LSTM+CRF model.
+        """
+        path = self.output + "seg_model_256_" + self.act
+        model = LstmCrf(
+            embedding_dim=50,
+            num_tags=3,
+            hidden_dim=256,
+            pretrained_emb=self.emb,
+            idx2tag=self.idx2tag,
+            path=path,
+        )
+        model = model.to(model.device)
+        self.valid_loss = model.fit(
+            epoch=50,
+            train_loader=self.data_dicts["train"],
+            eval_loader=self.data_dicts["validation"],
+            lr=0.001,
+            weight_decay=1e-6,
+        )
+        self.model = model
+        self.next(self.predict)
+
+    @step
+    def predict(self):
+        """
+        Evaluates the LSTM+CRF model in the test set
+        """
+        path = self.output + "seg_model_256_" + self.act
+        self.model.load_state_dict(torch.load(path))
+        results = self.model.evaluate(self.data_dicts["test"], opt="f1")
+        np.save(f"{self.output}/metrics_{self.act}", results)
+        self.next(self.end)
+
+    @step
+    def end(self):
+        """
+        Final step, save the trained model.
+        """
+        path = self.output + "seg_model_256_" + self.act
+        torch.save(self.model.state_dict(), path)
+
+
+if __name__ == "__main__":
+    SegmentationFlow()
