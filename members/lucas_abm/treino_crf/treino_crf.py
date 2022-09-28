b800c13bddaf1258d463fced85bda4cc540ff03e
ABMHub <lucasabmacedo@hotmail.com>
2022-09-28 10:46:01 -0300

    Teste CRF com cross validation

--- members/lucas_abm/treino_crf/treino_crf.py
+++ members/lucas_abm/treino_crf/treino_crf.py
@@ -1,8 +1,10 @@
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn_crfsuite.metrics import flat_classification_report, flat_f1_score
+from seqeval.metrics import classification_report, f1_score
 import sklearn_crfsuite
 import pandas as pd
 import nltk
+import numpy as np
 
 class CRF_Flow():
   X_train = None
@@ -15,25 +17,30 @@ class CRF_Flow():
   f1_score = 0
   result = ""
 
-  def __init__(self, csv_path : str = None, n_iterations : int = 50):
+  def __init__(self, csv_path : str = None, c1 = 0.17, c2 = 0.17, n_iterations : int = 50, cross_validation = False):
     """
     Construtor.
 
     Caso o "csv_path" seja incluído, o construtor chamará a função `self.run_all`
     """
+    self.c1 = c1
+    self.c2 = c2
+    self.n_iterations = n_iterations
     # nltk.download('punkt')
+
+    if csv_path is not None:
+      self.run_all(csv_path, cross_validation=cross_validation)
+
+  def init_architeture(self):
     self.model = sklearn_crfsuite.CRF(
       algorithm = 'lbfgs',
-      c1=0.17,
-      c2=0.17,
-      max_iterations=n_iterations,
+      c1=self.c1,
+      c2=self.c2,
+      max_iterations=self.n_iterations,
       all_possible_transitions=True
     )
 
-    if csv_path is not None:
-      self.run_all(csv_path)
-
-  def run_all(self, csv_path : str) -> None:
+  def run_all(self, csv_path : str, cross_validation = False) -> None:
     """
       Função para rodar todo o fluxo de uma vez:
 
@@ -44,12 +51,19 @@ class CRF_Flow():
 
       Esse método criará uma pasta chamada `results`
     """
-    self.load(csv_path)
-    self.train()
-    self.validation()
+    self.load(csv_path, split = not cross_validation)
+
+    if cross_validation is True:
+      self.cross_validation()
+
+    else:
+      self.init_architeture()
+      self.train()
+      self.validation()
+
     self.save("results/" + csv_path.split("/")[-1].split(".")[0] + ".txt")
 
-  def load(self, csv_path : str) -> None:
+  def load(self, csv_path : str, split = True) -> None:
     """Input: path para o arquivo csv contendo os dados
 
     carrega os arquivos IOB e
@@ -65,13 +79,47 @@ class CRF_Flow():
     y = y.apply(lambda y: y.split()).values
     X = X.apply(self.__get_features).values
 
-    self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    if split is True:
+      self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    else:
+      self.X_train = X
+      self.y_train = y
 
   def train(self) -> None:
     """
     Treina o modelo com os dados de treino guardados.
     """
     self.model.fit(self.X_train, self.y_train)
+
+  def cross_validation(self) -> None:
+    """
+    Valida o modelo fazendo a validação cruzada.
+
+    Dados gerados:
+
+    - `self.f1`: inteiro com o f1 score do modelo
+    - `self.result`: string com o classification report do modelo
+    """
+
+    predictions = np.array([], dtype="object")
+    true = np.array([], dtype="object")
+    tenfold = KFold(n_splits=10, shuffle=True, random_state=1)
+    i = 0
+    for train_index, test_index in tenfold.split(self.X_train):
+      print(f"Treino {i+1}/10")
+      self.init_architeture()
+      self.model.fit(self.X_train[train_index], self.y_train[train_index])
+
+      classes = list(self.model.classes_)
+      classes.remove('O')
+      predictions = np.concatenate([predictions, self.model.predict(self.X_train[test_index])], dtype="object")
+      true = np.concatenate([true, self.y_train[test_index]], dtype="object")
+      i+=1
+    # print(true[0:10])
+    # print(predictions[0:10])
+    self.result = classification_report(true, predictions)
+    self.f1 = f1_score(true, predictions)
 
   def validation(self) -> None:
     """
